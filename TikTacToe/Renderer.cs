a0192dec13876ae2e48712616058135079917d7d
sotiredofyours <h8everrything@gmail.com>
2022-10-22 16:21:28 +0400

    add TikTacToe game

---
+++ TikTacToe/Renderer.cs
@@ -0,0 +1,21 @@
+﻿namespace csharp_course;
+
+public class Renderer
+{
+    public void PrintGameBoard(FieldType[] a)
+    {
+        Console.Clear();
+        Console.WriteLine("\r/---+---+---\\");
+        for (int i = 0; i < 9; i+=3)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (a[j+i] == FieldType.Empty) Console.Write("|   ");
+                else Console.Write($"| {a[j+i]} ");
+            }
+            Console.WriteLine("|");
+            if (i != 6) Console.WriteLine("|---+---+---|");
+            else Console.WriteLine("\r\\---+---+---/");
+        }
+    }
+}
